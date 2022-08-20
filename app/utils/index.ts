b5be132c29a18e9a2cffac18ed111b101fc2d3a5
Sunday Ezeilo <ezeilo.su@gmail.com>
2022-08-20 14:07:09 +0100

    Use static private fields

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,7 +1,7 @@
 class SequentialIntegerGenerator {
-  private DEFAULT_START: number = 0;
-  private DEFAULT_END: number = Infinity;
-  private DEFAULT_STEP: number = 1;
+  private static DEFAULT_START: number = 0;
+  private static DEFAULT_END: number = Infinity;
+  private static DEFAULT_STEP: number = 1;
 
   private *generate(start: number, end: number, step: number) {
     for (let i = start; i <= end; i += step) {
@@ -12,16 +12,16 @@ class SequentialIntegerGenerator {
   private getConfig(config?: IDGeneratorConfig): Required<IDGeneratorConfig> {
     if (!config) {
       return {
-        end: this.DEFAULT_END,
-        step: this.DEFAULT_STEP,
-        start: this.DEFAULT_START
+        end: SequentialIntegerGenerator.DEFAULT_END,
+        step: SequentialIntegerGenerator.DEFAULT_STEP,
+        start: SequentialIntegerGenerator.DEFAULT_START
       };
     }
 
     return {
-      end: config.end || this.DEFAULT_END,
-      step: config.step || this.DEFAULT_STEP,
-      start: config.start || this.DEFAULT_START
+      end: config.end || SequentialIntegerGenerator.DEFAULT_END,
+      step: config.step || SequentialIntegerGenerator.DEFAULT_STEP,
+      start: config.start || SequentialIntegerGenerator.DEFAULT_START
     };
   }
 
